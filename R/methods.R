dcd0c81c96fa493513e7da1fe4c8c2178605c1d4
Serguei Sokol <sokol at insa-toulouse.fr>
2016-01-26 18:26:19 +0100

    fixed S4 method definition

--- R/methods.R
+++ R/methods.R
@@ -1,1 +1,8 @@
-solve.Rcpp_Rmumps <- function(a,b) if(missing(b)) a$inv() else a$solve(b)
+#solve.Rcpp_Rmumps <- function(a,b) if(missing(b)) a$inv() else a$solve(b)
+setMethod("solve",
+   signature(a = "Rcpp_Rmumps", b="ANY"),
+   function (a, b)
+   {
+      if(missing(b)) a$inv() else a$solve(b)
+   }
+)
