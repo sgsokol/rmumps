91d007e6026b5f611800991c74f4e2332684d8f5
Serguei Sokol <sokol@insa-toulouse.fr>
2019-07-03 16:43:03 +0200

    fixed broken NAMESPACE (roxygen2's fault); fixed export cnsts; extended copyright to 2019;

--- R/RcppExports.R
+++ R/RcppExports.R
@@ -7,11 +7,10 @@
 #' 
 #' @param s character scalar, name of the constant
 #' @return integer scalar, value of the sonstant
-#' @aliases RMUMPS_PERM_AMD RMUMPS_PERM_AMF RMUMPS_PERM_SCOTCH RMUMPS_PERM_PORD RMUMPS_PERM_METIS RMUMPS_PERM_QAMD RMUMPS_PERM_AUTO
+#' @aliases RMUMPS_PERM_AMD RMUMPS_PERM_AMF RMUMPS_PERM_SCOTCH RMUMPS_PERM_PORD RMUMPS_PERM_METIS RMUMPS_PERM_QAMD RMUMPS_PERM_AUTO cnsts
 #' @examples
 #' get_cnst("RMUMPS_PERM_AMD")
 #' @export
 get_cnst <- function(s) {
-    .Call('_rmumps_get_cnst', PACKAGE = 'rmumps', s)
+    .Call("_rmumps_get_cnst", s)
 }
-
--- src/get_cnst.cpp
+++ src/get_cnst.cpp
@@ -9,7 +9,7 @@
 //' 
 //' @param s character scalar, name of the constant
 //' @return integer scalar, value of the sonstant
-//' @aliases RMUMPS_PERM_AMD RMUMPS_PERM_AMF RMUMPS_PERM_SCOTCH RMUMPS_PERM_PORD RMUMPS_PERM_METIS RMUMPS_PERM_QAMD RMUMPS_PERM_AUTO
+//' @aliases RMUMPS_PERM_AMD RMUMPS_PERM_AMF RMUMPS_PERM_SCOTCH RMUMPS_PERM_PORD RMUMPS_PERM_METIS RMUMPS_PERM_QAMD RMUMPS_PERM_AUTO cnsts
 //' @examples
 //' get_cnst("RMUMPS_PERM_AMD")
 //' @export
